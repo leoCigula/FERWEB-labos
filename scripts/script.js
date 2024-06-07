2063c99fbcaccb0b0df02e89375a6095eed84fdb
leonardo.cigula@gmail.com <109247428+leoCigula@users.noreply.github.com>
2024-06-07 18:23:57 +0200

    JS mostly done

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ console.log(data);
 var kategorije = document.querySelector('#kategorije>ul');
 var kats=kategorije.querySelectorAll('p')
 var kategorija = document.querySelector('p.chosenCat');
+var cart = document.querySelector(".cart")
 
 
 kats.forEach(element => {
@@ -17,6 +18,18 @@ kats.forEach(element => {
     */
 });
 
+/*
+cart.addEventListener('mouseover',function funkcija(){
+    if()
+    let cnt = document.querySelector(".numOfItems");
+    cnt.style.display = "block";
+});
+cart.addEventListener('mouseleave',function funkcija(){
+    let cnt = document.querySelector(".numOfItems");
+    cnt.style.display = "none";
+});
+
+*/
 
 
 function postaviKategoriju(el){
@@ -30,7 +43,7 @@ function postaviKategoriju(el){
     kategorija.style.color = "red";
     kategorija.style.fontSize = "larger";
 
-    proizv.innerHTML = ' ';
+    proizv.innerHTML= '';
     console.log(index); 
     addItem(index);
 
@@ -43,26 +56,94 @@ function addItem(kategorija){
     let proiz = document.querySelector(".proizvodi");
     let div = document.createElement("div");
     let info = data["categories"][kategorija]["products"];
-    let name;
-    let image;
     info.forEach(element => {
+        let name;
+        let image;
         name = element["name"];
         image = element["image"]
         console.log(name);
         console.log(image);
+        div = null;
         div = document.createElement("div");
         div.classList="proizvod";
-        div.data=1;
+        div.setAttribute("data",name);
         div.innerHTML = 
         `<img src = ${image}>\n `+
         `<p> ${name}</p>\n `+
          `  <div class="numOfItem">  <p id="num">0</p> </div>  \n `;
-        div.onclick = function call(){
-            alert("stavljeno u cart")
-        }
+
+         div.addEventListener('click', function() {
+            // Get the current count from localStorage, defaulting to 0 if not found
+            let count = parseInt(localStorage.getItem(name)) || 0;
+            count++;
+            // Update localStorage with the new count
+            localStorage.setItem(name, count);
+            // Update the displayed count
+            this.querySelector("#num").textContent = count;
+            updateCounter();
+        });
+
         proiz.appendChild(div); 
     
     });
    
 }
 
+function updateCounter(){
+    let cnt = document.querySelector(".numOfItems>p#num");
+    let count = cnt.innerText;
+    let newCnt=0;
+    let variable ; 
+    if(count == 0)
+        document.querySelector(".numOfItems").style.display="none";
+    else 
+        document.querySelector(".numOfItems").style.display="block";
+    for(let i=0;i<10;i++){
+        for(let j=0;j<5;j++){
+            variable = data["categories"][i]["products"][j]["name"];
+            let medu = parseInt(localStorage.getItem(variable)) || 0;
+            newCnt = newCnt + medu;
+        }
+    }
+    if(newCnt > 0){
+        document.querySelector(".numOfItems").style.display="block";
+        cnt.innerText = newCnt;
+    }
+}
+/*
+function addItem(kategorija) {
+    let proiz = document.querySelector(".proizvodi");
+    let info = data["categories"][kategorija]["products"];
+
+    info.forEach(element => {
+        let name = element["name"];  // Declare with `let` to ensure block scope
+        let image = element["image"];
+        console.log(name);
+        console.log(image);
+
+        let div = document.createElement("div");
+        div.classList = "proizvod";
+        div.setAttribute("data", name);
+        div.innerHTML = 
+        `<img src="${image}">\n` +
+        `<p>${name}</p>\n` +
+        `<div class="numOfItem"><p id="num">0</p></div>\n`;
+
+        // Use an IIFE to create a closure that captures the current `name` value
+        (function(name) {
+            div.addEventListener('click', function() {
+                // Get the current count from localStorage, defaulting to 0 if not found
+                let count = parseInt(localStorage.getItem(name)) || 0;
+                count++;
+                // Update localStorage with the new count
+                localStorage.setItem(name, count);
+                // Update the displayed count
+                this.querySelector("#num").textContent = count;
+            });
+        })(name);
+
+        proiz.appendChild(div);
+    });
+}
+
+*/
